74490c4675ace02414c7ca8a3ef6ad44fae398c2
Heidymoreno26 <heimoreno26@gmail.com>
2025-05-23 21:30:00 -0500

    Add files via upload

---
+++ DataLake/Scripts/DDL_Departamentos.sql
@@ -0,0 +1,12 @@
+-- DataLake.dbo.DL_Departamentos definition
+
+-- Drop table
+
+-- DROP TABLE DataLake.dbo.DL_Departamentos;
+
+CREATE TABLE DataLake.dbo.dl_departamentos (
+	COD_DPTO tinyint NULL,
+	NOM_DPTO nvarchar(60) NULL,
+	LATITUD float NULL,
+	LONGITUD float NULL,
+	GeoDepartamento nvarchar(35) NULL
